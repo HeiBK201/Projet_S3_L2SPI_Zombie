821b29bbc6b0a02cd93a6cbd0e87edb1289eb2c9
HeiBK201 <sadonnet_du61@hotmail.fr>
2017-01-09 09:03:22 +0100

    Maj

--- Sauvegarde.h
+++ Sauvegarde.h
@@ -1,16 +1,16 @@
 #include "Outil.h"
 
 
-//Fonction qui va initaliser les fichiers de sauvegarde
+// Fonction qui va initaliser les fichiers de sauvegarde.
 void initFichiers();
 
-//
+// Fonction qui va ouvrir les fichiers en mode "écriture".
+void fichiersEnregistrer();
 
+// Fonction qui va ouvrir les fichiers en mode "lecture".
+void fichiersCharger();
 
-//
-
-
-//
-
+// Fonction qui va fermer les fichiers
+void fermerFichiers();
 
 //
